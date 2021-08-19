c2a110a21ed3ce10e1bb94e3a13bc67de545a30e
Iseul Park <lovelyangelapark@gmail.com>
2021-08-19 12:51:25 +0900

    20210819 중간저장

---
+++ 4-react/myworkspace/src/components/Calculator.tsx
@@ -0,0 +1,24 @@
+// Calculator 컴포넌트
+
+
+// 1. 입력버튼을 클릭하면 prompt로 입력값을 두번 받음
+// 입력값 1 / 입력값 2
+// a = prompt, b = prompt
+
+// 2. 그 다음 연산자를 prompt로 받음
+// +, -, *, /
+
+// 3. 입력값 두개에 대한 연산 결과를 화면에 출력
+// state를 사용해야 함
+
+// 예) 입력값1 : 10
+//     입력값2: 5
+//     연산자 : *
+//     결과값 : 50
+//      <div>50</div>
+
+const Calculator = () => {
+  return <></>;
+};
+
+export default Calculator;
