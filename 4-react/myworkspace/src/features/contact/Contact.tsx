4e52254f1a27630c6e1ecdfee6ddad29513b8488
Iseul Park <lovelyangelapark@gmail.com>
2021-09-21 22:20:45 +0900

    20210921

---
+++ 4-react/myworkspace/src/features/contact/Contact.tsx
@@ -0,0 +1,65 @@
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { RootState } from "../../store";
+
+const getTimeString = (unixtime : number) => {
+   const dateTime = new Date(unixtime);
+  return `${dateTime.toLocaleDateString([],{month:'2-digit',day:'2-digit'})} ⏱${dateTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false})}`;
+}
+// 하루 지나면 월/일 표시하게
+// 하루 안지나면 시간만 표시하게
+
+const Contact = () => {
+  const contact = useSelector((state: RootState) => state.contact);
+  const history = useHistory();
+
+  return (
+        <div style={{width:"70vw"}} className="mx-auto">
+     <h2 className="text-center my-4"><b>Contacts</b>📞</h2>
+
+    <div className="d-flex justify-content-end mb-2">
+              <button
+            type="button"
+            className="btn btn-dark text-nowrap btn-sm p-2"
+            onClick={() => {
+              history.push("/contact/create");
+            }}
+          >
+            <i className="bi bi-person-plus-fill me-2"></i>
+            추가
+          </button>
+    </div>
+
+    <table className="table table-striped mt-4 table table-hover">
+      <thead className="display-flex;">
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Telephone</th>
+          <th>Email</th>
+          <th>Date Created</th>
+        </tr>
+      </thead>
+
+      <tbody className="tbody">
+{contact.data.map((item, index) => (
+ <tr className="display-flex">
+          <td className="text-center">{item.select}</td> 
+            <td 
+            style={{ cursor: "pointer" }}
+             onClick={()=> {
+              history.push(`/contact/detail/${item.id}`);
+            }}
+            >{item.txtName}</td>
+             <td>{item.txtContact}</td>       
+            <td>{item.txtEmail}</td>
+            <td>{getTimeString(item.createTime)}</td>
+        </tr>
+))}
+      </tbody>
+    </table>  
+    </div>
+  );
+};
+
+export default Contact;
---
+++ 4-react/myworkspace/src/features/contact/ContactDetail.tsx
@@ -0,0 +1,100 @@
+import {useParams} from "react-router-dom";
+
+import { useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { AppDispatch, RootState } from "../../store";
+import { removeContact } from "./contactSlice";
+
+
+const ContactDetail = () => {
+  const { id } = useParams<{ id : string } >();
+
+  const ContactItem = useSelector((state: RootState) =>
+    state.contact.data.find((item) => item.id === +id)
+  );
+
+
+  const dispatch = useDispatch<AppDispatch>();
+  const history = useHistory();
+
+  const handleAddClick = () => {
+        dispatch(removeContact(+id));
+        history.push("/contact");
+  };
+
+  return (
+    <div style={{width:"40vw"}} className="mx-auto">
+     <h2 className="text-center my-4 mb-5">Contact Detail 📃</h2>
+     <form className="mx-auto">
+    {ContactItem && (
+    <table className="table">
+<tbody>
+         <tr>
+          <th>성별</th>
+          <td>{ContactItem.select}</td>
+      </tr>
+       <tr>
+          <th>이름</th>
+      <td>{ContactItem.txtName}</td>
+      </tr>
+
+      <tr>
+          <th>전화번호</th>
+     <td>{ContactItem.txtContact}</td>
+      </tr>
+
+      <tr>
+            <th>이메일</th>
+     <td>{ContactItem.txtEmail}</td>
+        </tr>
+
+        <tr>
+            <th>메모</th>
+     <td>{ContactItem.memo}</td>
+
+        </tr>
+
+      </tbody>
+    </table>
+    )}
+  </form>
+
+      <div className="mt-3">
+        <button
+          className="btn btn-light border border-2 btn-sm p-2 float-start"
+          onClick={() => {
+            history.push("/contact");
+          }}
+        >
+          <i className="bi bi-list me-1"></i>
+          목록
+        </button>
+        <button
+          className="btn btn-dark btn-sm p-2 float-end"
+          onClick={() => {
+            handleAddClick();
+          }}
+       >
+         <i className="bi bi-trash me-1"></i>
+          삭제
+        </button>
+
+        <button
+          className="btn btn-warning btn-sm p-2 float-end me-1"
+            onClick={() => {
+              history.push(`/contact/edit/${id}`);
+            }}
+       >
+         <i className="bi bi-pencil-square me-1"></i>
+          <b>수정</b>
+        </button>
+        
+      </div>
+
+</div>
+
+  )
+}
+
+export default ContactDetail;
